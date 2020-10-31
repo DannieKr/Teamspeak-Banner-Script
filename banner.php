343aee68e5122d3a44fdb17516974aac4e9c9aac
DannieKr <dannie.kro@gmail.com>
2020-10-31 15:01:43 +0100

    some changes

--- banner.php
+++ banner.php
@@ -1,11 +1,12 @@
 <?php
-$pictureDirecotry  = 'pictures/*';
+# set your picture directory, where all your pictures are located in
+$pictureDirecotry  = '/var/www/public/tsscript/pictures/*';
 
 header('Content-Type: image/png');
 
 $pictureList = glob($pictureDirecotry);
-
 shuffle($pictureList);
 $picture = file_get_contents($pictureList[0]);
+
 echo $picture;
 ?> 
